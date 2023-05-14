246a686436c933d541aaaeab864e1d7a04b523b3
devemdobro <92488788+devemdobro@users.noreply.github.com>
2023-05-13 21:41:56 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+/*
+	O que precisamos fazer? - quando clicarmos no botão de troca de tema temos que alterar a cor do tema da página para as cores do tema claro ou do tema escuro
+
+	- objetivo 1 - quando clicar no botão de troca de tema, adicionar a classe modo-escuro no body pra que os estilos do modo escuro sejam aplicados e mudar a imagem pra lua
+        - passo 1 - pegar no JS o elemento HTML correspondente ao botão de troca de tema
+        - passo 2 - dar um jeito de pegar no JS o elemento HTML corresponde ao body
+        - passo 3 - dar um jeito de identificar o clique do usuário no botão de troca de tema
+        - passo 4 - adicionar a classe modo-escuro no body
+        - passo 5 - trocar a imagem do botão de alterar tema pra lua
+    
+	- objetivo 2 - quando clicar no botão de troca de tema, caso o body já tenha a classe modo-escuro, remover a classe pra mudar pro modo claro e mudar a imagem pro sol
+        - passo 6 - verificar se o body já tem a classe modo-escuro
+        - passo 7 - remover a classe do modo-escuro do body
+        - passo 8 - trocar a imagem do botão de alterar tema pra sol
+*/
+
+// objetivo 1 - quando clicar no botão de troca de tema, adicionar a classe modo-escuro no body pra que os estilos do modo escuro sejam aplicados e mudar a imagem pra lua
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
